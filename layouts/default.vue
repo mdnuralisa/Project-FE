42d34f7f80dc3774dcaa9ac651d79d18949373a5
Lisa <md.nuralisa@gmail.com>
2023-11-04 15:31:39 +0800

    new commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
-        header 1
-      <AppHeader />
+      <ul class="nav nav-pills nav-fill sticky-top bg-white">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inventories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Log Out</a>
+        </li>
+      </ul>
       <slot />
-      <AppFooter />
+      
     </div>
   </template>
   
