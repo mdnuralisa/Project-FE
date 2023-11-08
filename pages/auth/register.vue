b4da6ed38d624f25908f8e58e1f095a109a16227
Lisa <md.nuralisa@gmail.com>
2023-11-08 22:56:47 +0800

    test register

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -20,12 +20,12 @@ definePageMeta({
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3 text-center">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
             </div>
             <div class="mb-3 text-center">
-                <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword2">
+                <label for="confirmPassword" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmPassword">
             </div>
             <div class="mb-3 text-center">
                 <button type="submit" class="btn btn-primary">Create your account</button>
