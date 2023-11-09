266f59fdd97ab00378388a47ac998caca9c9a342
Lisa <md.nuralisa@gmail.com>
2023-11-09 23:20:49 +0800

    change to tailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,57 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/bootstrap.scss'],
-  plugins: [
-    { src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js', mode: 'client'}
-  ]
+  // css: ['~/assets/css/bootstrap.scss'],
+  // plugins: [
+  //   { src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js', mode: 'client'}
+  // ]
+  modules: [    '@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    config: {
+      darkMode: 'class',
+      theme: {
+        extend: {
+          colors: {
+            primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+          }
+        },
+        fontFamily: {
+          'body': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+      ],
+          'sans': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+      ]
+        }
+      }
+    } 
+  }
 })
