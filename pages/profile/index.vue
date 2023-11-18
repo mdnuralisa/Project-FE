a95834f1d324ec10ff43119d4839fb2788277d72
Lisa <md.nuralisa@gmail.com>
2023-11-18 14:02:37 +0800

    add profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,26 +1,73 @@
-<template>
+<script setup lang="ts">
+import { nextTick } from 'vue'
+
+
+let form = ref({
+    email: ""
+})
+
+const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+
+const updateData = () => {
     
-    <div class="w-50 mx-auto mt-5 border border-primary border-2 p-3">
-        <form>
-            <div class="position-relative m-5 text-center">
-                <h1 class="d-inline">Profile</h1>
-            </div>
-            <div class="mb-3 text-center">
-                <label for="exampleInputEmail1" class="form-label">Your registered email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3 text-center">
-                <label for="exampleInputPassword1" class="form-label">Change Password?</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 text-center">
-                <label for="exampleInputPassword2" class="form-label">Confirm New Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword2">
-            </div>
-            <div class="mb-3 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-    </div>
+    nextTick(async () => {
+        
+        const { data, pending, error, refresh } = await useFetch('http://localhost:8080/show/',{
+            method: "get",
+            
+            headers: {
+                
+                Authorization: `Bearer ${token}`,
+            },
+            onResponse({ request, response, options }) {
+                console.log(response);
+                // Process the response data
+                
+                form.value = response._data.data
+                // window.$cookies.set('token', response._data.data.token);
+                
+            },
+            onResponseError({ request, response, options }) {
+                console.log(response);
+                // Handle the response errors
+            }
+        });
+    })
+}
+
+onMounted(() => {
+    updateData()
+});
+
+</script>
+
+<template>
+<section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+          Flowbite    
+      </a>
+      <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Change Password
+          </h2>
+          <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+              <div>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your registered email</label>
+                  <input v-model="form.email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
+              </div>
+              <div>
+                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                  <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+              </div>
+              <div>
+                  <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                  <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="mb-4 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+              </div>
+              <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Reset passwod</button>
+          </form>
+      </div>
+  </div>
+</section>
 </template>
